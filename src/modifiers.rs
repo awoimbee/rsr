844bf09f7d5a30aa07778825549108c8b26fa4f8
Arthur Woimbée <arthur.woimbee@gmail.com>
2020-06-27 23:11:34 +0200

    simplify modifiers

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -2,22 +2,6 @@ use std::borrow::Cow;
 
 pub type DynFnPtr = &'static (dyn (Fn(&str) -> Cow<str>) + Sync);
 
-struct Modifier {
-    s: &'static str,
-    fn_ptr: DynFnPtr,
-}
-
-const MODIFIERS: [Modifier; 2] = [
-    Modifier {
-        s: "U",
-        fn_ptr: &to_upper,
-    },
-    Modifier {
-        s: "L",
-        fn_ptr: &to_lower,
-    },
-];
-
 fn to_upper(s: &str) -> Cow<str> {
     Cow::from(s.to_ascii_uppercase())
 }
@@ -27,10 +11,9 @@ fn to_lower(s: &str) -> Cow<str> {
 }
 
 pub fn get_modifier(requested: &str) -> Option<DynFnPtr> {
-    for m in MODIFIERS.iter() {
-        if requested == m.s {
-            return Some(m.fn_ptr);
-        };
+    match requested {
+        "U" => Some(&to_upper),
+        "L" => Some(&to_lower),
+        _ => None,
     }
-    None
 }
